81cfd0249dd796a5fcee5e14e5042d36bc3af8f5
Vanderleicio <68967481+Vanderleicio@users.noreply.github.com>
2024-02-14 19:13:51 -0300

    Lógicas de colisão

--- main.c
+++ main.c
@@ -1,73 +1,167 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <signal.h>
-#include <unistd.h>
-#include <string.h>
-#include <time.h>
-#include <intelfpgaup/KEY.h>
-#include <intelfpgaup/video.h>
 
-int screen_x, screen_y;
+// x1 e y1 é o canto inferior esquerdo
+// x2 e y2 é o oposto
+struct bola {
+    int x1;
+    int y1;
+    int x2;
+    int y2;
+    int vel[2];
+    int cor;   
+};
 
-int char_x, char_y;
+// x1 e y1 é o canto inferior esquerdo
+// x2 e y2 é o oposto
+// Tamanho de cada bloco 53px x 20px
+struct bloco{
+    int x1;
+    int y1;
+    int x2;
+    int y2;
+    int cor;
+    int existe;
+    void (*acelBolaBloco)(struct bloco, struct bola);
+};
 
-void gen_line (int *, int *, int *, int *, unsigned *);
+//x1 é o começo e x2 o fim da barra
+struct barra{
+    //Zonas da barra: Esquerda (0, 12); Centro (13, 16); Direita (17, 29)
+    int x1;
+    int x2;
+    int y;
+    int cor;
+    void (*acelBolaBarra)(struct barra, struct bola);
+};
 
-void print_text (int, int, int, int, unsigned);
-
-volatile sig_atomic_t stop;
-
-void catchSIGINT(int signum){
-    stop = 1;
+// Função de soma
+void acelBolaBloco(struct bloco brick, struct bola ball) {
+    if (ball.y2 == brick.y1){
+        //Batida inferior
+        ball.vel[1] = 1; 
+    } else if(ball.y1 == brick.y2){
+        //Batida superior
+        ball.vel[1] = -1;
+    } else if(ball.x1 == brick.x2){
+        //Batida na direita
+        ball.vel[0] = 1;
+    }else if(ball.x2 == brick.x1){
+        ball.vel[0] = -1;
+    }
 }
 
+// Função de subtração
+void acelBolaBarra(struct barra bar, struct bola ball) {
+    if ((ball.x1 >= bar.x1) && (ball.x1 <= bar.x1 + 12)){
+        ball.vel[0] = -1;
+        ball.vel[1] = -1;
+    } else if ((ball.x1 >= bar.x1 + 13) && (ball.x1 <= bar.x1 + 16)){
+        ball.vel[0] = 0;
+        ball.vel[1] = -1;
+    } else if ((ball.x1 >= bar.x1 + 17) && (ball.x1 <= bar.x2)){
+        ball.vel[0] = 1;
+        ball.vel[1] = -1;
+    }
+}
 
-/* This program draws randomly-generated lines on the VGA display. The code uses
-* the character device drivers IntelFPGAUP/video and IntelFPGAUP/KEY. It draws a
-* random line each time a pushbutton KEY is pressed. The coordinates of each new
-* line are displayed on the bottom right of the screen. Exit by typing ^C. */
-int main(int argc, char *argv[]) {
-    int KEY_data;
+void moverBola(struct bola ball){
+    ball.x1 = ball.x1 + ball.vel[0];
+    ball.x2 = ball.x2 + ball.vel[0];
+    ball.y1 = ball.y1 + ball.vel[1];
+    ball.y2 = ball.y2 + ball.vel[1];
+}
+
+int main() {
+    //Inicializando os blocos, serão 6 por linha, 4 linhas.
+    int perdeu = 0;
     int x1, y1, x2, y2;
-    unsigned color;
-    char msg_buffer[80];
-    time_t t; // used to seed the rand() function
-    // catch SIGINT from ^C, instead of having it abruptly close this program
-    //signal(SIGINT, catchSIGINT);
-    srand ((unsigned) time(&t)); // seed the rand function
-    // Open the character device drivers
-    if (!KEY_open ( ) || !video_open ( ))
-        return -1;
-    video_read (&screen_x, &screen_y, &char_x, &char_y); // get screen & text size
-    video_erase ( ); // erase any text on the screen
-    // set random initial line coordinates
-    gen_line (&x1, &y1, &x2, &y2, &color);
-    /* There are two VGA buffers. The one we are drawing on is called the Back
-    * buffer, and the one being displayed is called the Front buffer. The function
-    * video_show swaps the two buffers, allowing us to display what has been drawn
-    * on the Back buffer */
-    video_clear ( ); // clear current VGA Back buffer
-    video_show ( ); // swap Front/Back to display the cleared buffer
-    video_clear ( ); // clear the VGA Back buffer, where we will draw lines
-    video_line (x1, y1, x2, y2, color);
-    video_show ( ); // swap Front/Back to display the line
+    int numBlocos = 24;
+    int x1_in = 1;
+    int y1_in = 20;
+    int auxx1 = 0;
+    int auxy1 = 0;
+    int passo_x = 53; //Tamanho X do bloco
+    int passo_y = 20; //Tamanho Y do bloco
+    struct barra barra;
+    struct bloco blocos[numBlocos];
+    
+    for (int i = 0; i < numBlocos; i++){
+        x1 = x1_in + (passo_x * auxx1++);
+        y1 = y1_in + (passo_y * auxy1);
+        x2 = x1 + (passo_x - 1);
+        y2 = y1 - (passo_y - 1);
 
-    video_close ( );
-    KEY_close ( );
-    printf ("\nExiting sample program\n");
+        if (auxx1 == 5){
+            auxx1 = 0;
+            auxy1++;
+        }
+
+        blocos[i].x1 = x1;
+        blocos[i].y1 = y1;
+        blocos[i].x2 = x2;
+        blocos[i].y2 = y2;
+        blocos[i].existe = 1;
+        blocos[i].cor = 1;
+    }
+
+    //Fim da inicialização dos blocos
+
+    //Inicialização da bola
+
+    struct bola bola;
+    bola.x1 = 40;
+    bola.x2 = 41;
+    bola.y1 = 20;
+    bola.y2 = 21;
+    bola.vel[0] = 0;
+    bola.vel[1] = 1;
+
+    //Fim da inicialização da bola
+
+    //Inicialização da barra
+
+
+    //Fim da inicialização da barra
+
+    //Inicialização dos limites
+    int limite_x1= 0;
+    int limite_y1= 239;
+    int limite_x2= 319;
+    int limite_y2= 0;
+
+    while (1){
+        moverBola(bola);
+        
+        //Checar choque com as laterais
+        if (bola.x1 <= limite_x1){
+            bola.vel[0] = 1;
+        } else if(bola.x2 >= limite_x2){
+            bola.vel[0] = -1;
+        } else if(bola.y2 >= limite_y2){
+            bola.vel[1] = 1;
+        } else if(bola.y1 >= limite_y1){
+            perdeu = 1;
+        } 
+
+        for (int i = 0; i < numBlocos; i++){
+            if(blocos[i].existe){
+
+                if (((bola.x1 >= blocos[i].x1 && bola.x1 <= blocos[i].x2) && 
+                (bola.y1 >= blocos[i].y1 && bola.y1 <= blocos[i].y2)) ||
+                ((bola.x2 >= blocos[i].x1 && bola.x2 <= blocos[i].x2) && 
+                (bola.y2 >= blocos[i].y1 && bola.y2 <= blocos[i].y2))){
+                    //Houve colisão com o bloco
+                    blocos[i].existe = 0; //Bloco deixa de existir
+                    blocos[i].acelBolaBloco(blocos[i], bola); //Bola muda de direção
+                }
+            }
+        }
+
+        if (bola.x1 >= barra.x1 && bola.x1 <= barra.x2 && bola.y1 == barra.y){
+            barra.acelBolaBarra(barra, bola);
+        }
+    }
+
+    printf("Hello to, world!\n");
     return 0;
-}
-
-/* Generate a new random line and change the values by referer*/
-void gen_line (int *x1, int *y1, int *x2, int *y2, unsigned *color) {
-    
-    unsigned int video_color[] = {video_WHITE, video_YELLOW, video_RED,
-    video_GREEN, video_BLUE, video_CYAN, video_MAGENTA, video_GREY,
-    video_PINK, video_ORANGE};
-    
-    *x1 = rand()%(screen_x - 1); // random x position
-    *y1 = rand()%(screen_y - 1); // random y position
-    *x2 = rand()%(screen_x - 1); // random x position
-    *y2 = rand()%(screen_y - 1); // random y position
-    *color = video_color[(rand()%10)]; // random out of 10 video colors
 }

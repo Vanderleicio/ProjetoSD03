b936805daafc0b6df9e3985b01b8290bd632cbc2
Washington Oliveira <wdeoliveiraj@gmail.com>
2024-02-17 17:06:28 -0300

    correções e comentários adicionados

--- mainv2.c
+++ mainv2.c
@@ -8,6 +8,8 @@
 #include <intelfpgaup/video.h>
 #include <intelfpgaup/accel.h>
 
+
+// ====================================================== BLOCO DE 'OBJETOS' ATRIBUTOS ====================================================== //
 typedef struct {
     int screen_x, screen_y;
     int char_x, char_y;
@@ -17,6 +19,7 @@ typedef struct {
     int pos_x;
     int pos_y;
     int dir; //Não sei o que seria isso, então vou criar o vel embaixo (att: Vanderleicio)
+             // Era o ângulo da bola, seria mais fácil ter implementado com o ângulo (att: Washington)
     int vel[2];
     unsigned color;
 } Bola;
@@ -34,7 +37,6 @@ typedef struct {
     int existe;
     unsigned color;
 } Bloco;
-//NIRVAN
 
 typedef struct {
     int pos_x1;    // Extremidade esquerda da barra
@@ -45,16 +47,6 @@ typedef struct {
 } Barra;
 
 
-//
-void erase_pos_Bola_previous(){
-    //video_box(aux_x1-1,aux_y1-1,aux_x2-1,aux_y2-1,0);
-
-}
-
-//
-void erase_pos_Bloco(){
-
-}
 // ====================================== BLOCO DE FUNÇÕES PARA AS CONFIGURAÇÕES INICIAIS ==========================================//
 /* Constrói as bordas da tela
     Param:
@@ -80,26 +72,27 @@ void build_cage(Tela *tela, unsigned color){
         - pos_x2 -> Posição da extremidade direita da barra
   
 */
-void erase_pos_Barra_previous(int pos_x1, int pos_x2){
-    video_line(pos_x1, 235, pos_x2, 235, 0);
-}
+//void erase_pos_Barra_previous(int pos_x1, int pos_x2){
+//    video_line(pos_x1, 235, pos_x2, 235, 0);
+//}
 
 
 /* Atualiza a posição da barra, no buffer, com base na leitura de acelerômetro
     Param:
         - barra -> Objeto Barra
+        - tela -> Objeto Tela
 */
 void update_pos_Barra(Barra *barra, Tela *tela){
     int ac_ready, ac_tap, ac_dtap, x_val, y_val, z_val, mgper;
-    // Apaga a posição anterior antes de atualizar
-    erase_pos_Barra_previous((*barra).pos_x1, (*barra).pos_x2);
+    // Apaga a posição anterior antes de atualizar (NÃO PRECISA MAIS)
+    //erase_pos_Barra_previous((*barra).pos_x1, (*barra).pos_x2);
 
     // Realiza a leitura do acelerômetro
     accel_read(&ac_ready, &ac_tap, &ac_dtap, &x_val, &y_val, &z_val, &mgper);
-    // Faz a calibragem para não reduzir o valor e utilizar diretamente como movimento da barra
+    // Faz a calibragem para reduzir o valor e utilizar diretamente como movimento da barra
 	x_val = x_val/12;
 
-    // Trecho de código para fazer a saturação e não deixar a bola passar das extremidades
+    // ++++++++ Trecho de código para fazer a saturação e não deixar a bola passar das extremidades ++++++++ //
 	if ((*barra).pos_x2 + x_val >= (*tela).screen_x - 2){ // Para a bola não passar do lado direito da tela
 		(*barra).pos_x2 = (*tela).screen_x - 2;
 		(*barra).pos_x1 = ((*tela).screen_x - 2) - ((*barra).width);// 
@@ -112,10 +105,12 @@ void update_pos_Barra(Barra *barra, Tela *tela){
 		(*barra).pos_x1 = (*barra).pos_x1 + x_val;
 		(*barra).pos_x2 = (*barra).pos_x2 + x_val;
 	}
-    // Escreve no buffer
+    // Escreve no buffer a nova posição da barra
     video_line((*barra).pos_x1, (*barra).pos_y, (*barra).pos_x2, (*barra).pos_y, (*barra).color);
 }
 
+
+// -------- --------- -------- QUE TIPO DE MONSTRUOSIDADE É ESTA? -------- --------- -------- //
 void acelBolaBarra(Barra *bar, Bola *ball) {
     int lado = ((*bar).width/2) - 2;
     
@@ -131,6 +126,7 @@ void acelBolaBarra(Barra *bar, Bola *ball) {
     }
 }
 
+// -------- --------- -------- QUE TIPO DE MONSTRUOSIDADE É ESTA? -------- --------- -------- //
 void acelBolaBloco(Bloco *brick, Bola *ball) {
     if (((*ball).pos_y) == (*brick).pos_y2){
         //Batida inferior
@@ -149,77 +145,75 @@ void acelBolaBloco(Bloco *brick, Bola *ball) {
     }
 }
 
-// Atualiza a posição da bola
-void update_pos_Bola(){
 
-}
-
-
-//Verifica se a bola bateu em alguma coisa 
-void control_colision(){
-
-}
-
-void moverBola(Bola *ball){
-    (*ball).pos_x = (*ball).pos_x + (*ball).vel[0];
-    (*ball).pos_y = ((*ball).pos_y + (*ball).vel[1]);
+/* Atualiza a posição da bola, no buffer, com base em sua velocidade. 
+    Param:
+        - bola -> Objeto Bola
+*/
+void update_pos_Bola(Bola *bola){
+    (*bola).pos_x = (*bola).pos_x + (*bola).vel[0];
+    (*bola).pos_y = ((*bola).pos_y + (*bola).vel[1]);
+    // Escreve no buffer
+    video_box((*bola).pos_x, (*bola).pos_y, ((*bola).pos_x + 1), ((*bola).pos_y + 1), (*bola).color);
 }
 
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 int main(){
-	int  KEY_data; 
-	int control = 1;// Permanece jogando
+	int botao_valor;// Utiliizado para descobrir o botão pressionado
+	// int control = 1;// Permanece jogando
     Tela tela;//Ok
     Bola bola;
     Barra barra;//Ok
-    Bloco vetor[40]; //6 blocos por linha; 4 linhas
+    
+    int numBlocos = 40;// 10 blocos por linha e 4 linhas 
+    int comprimento = 30; //Tamanho X do bloco
+    int altura = 10; //Tamanho Y do bloco
+    Bloco vetor[numBlocos];
 
     unsigned int video_color[] = {video_WHITE, video_YELLOW, video_RED,
     video_GREEN, video_BLUE, video_CYAN, video_MAGENTA, video_GREY,
     video_PINK, video_ORANGE};
     
-    // Inicializando a tela vga
-	if (!KEY_open ( ) || !video_open ( ))
-	return -1;
-	
+    // Inicializando os devices
+	if (!KEY_open ( ) || !video_open ( ) || !accel_open()){
+	    return -1;
+    }
+
     // Obtendo params da tela
     video_read(&tela.screen_x, &tela.screen_y, &tela.char_x, &tela.char_y);
     // Limpo qualquer coisa que possa ter na tela
     video_erase();
-    video_clear ( ); // clear current VGA Back buffer
-    video_show ( ); // swap Front/Back to display the cleared buffer
-    video_clear ( ); // clear the VGA Back buffer, where we will draw lines
+    video_clear ( );
+    video_show ( ); 
+    video_clear ( );
 
     // Inicializando o Acelerômetro
-    accel_open();
-    accel_init ();
-    accel_calibrate ();
-    accel_format (1, 8);
+    accel_init();
+    accel_calibrate();
+    accel_format(1, 8);
 
     // Colocando os objetos nas posições iniciais
     build_cage(&tela, video_color[1]);// Constrói a gaiola
+
     // =========== Constrói a barra
     barra.width = 40;
     barra.pos_y = 235;
     barra.color = video_color[1];
     barra.pos_x1 = ((tela.screen_x -2)/2) - (barra.width/2);
     barra.pos_x2 = barra.pos_x1 + barra.width - 1;
+
     // =========== Constrói a bola
     bola.color = video_color[1];
-    bola.dir = 12;
+    bola.dir = 270;
+    // Se tivesse usado o bola.dir, os vetores de velocidade seriam vy = sen(dir) e vx = cos(dir)    (att: Washington)
     bola.vel[0] = 0;
     bola.vel[1] = 1;
-    bola.pos_x = 160;//rand() % (tela.screen_x-2);// Gera a bola em uma posição aleatória no eixo x
-    bola.pos_y =  tela.screen_y - 100;// Posição fixa em y 
+    bola.pos_x = 160;
+    bola.pos_y =  tela.screen_y - 100;
     
-    // Constrói os blocos
-    int numBlocos = 40;//40;// 10 blocos por linha e 4 linhas 
-    int comprimento = 30; //Tamanho X do bloco
-    int altura = 10; //Tamanho Y do bloco
-	
-    
+    // =========== Constrói os blocos
     int cont = 1;
     vetor[0].pos_x1 = 1;
     vetor[0].pos_y1 = 1;
@@ -252,46 +246,42 @@ int main(){
          
     }
 
+    // ::::: ??????????????????? :::::MUDAR PARA CALCULOS ENVOLVENDO O TELA. //
     int limite_x1= 1; //Pra bola não entrar na linha
     int limite_y1= 1;
     int limite_x2= 318;
     int limite_y2= 238;
+    // ::::: ??????????????????? ::::: //
 
-	/// INICIAL LOGO DEPOIS DE CONSTRUIR A TELA
-	KEY_read (&KEY_data);
+	/// Deixa o jogo travado no inicio até que pressione o botão da esquerda
+	KEY_read (&botao_valor);
 	video_text (tela.char_x/2, tela.char_y/2, "Pressione o botao Iniciar");
-	while(KEY_data != 8){// Só inicia o loop principal quando pressiona o botão 
-		KEY_read (&KEY_data);
+	while(botao_valor != 8){// Só inicia o loop principal quando pressiona o botão 
+		KEY_read (&botao_valor);
 	}
 	video_erase();
 
 
     // Loop principal do jogo
     while (1){
-    		KEY_read (&KEY_data);
-    		if(KEY_data == 4){// Aperto botão pausar
-			// PAUSAR
-				video_text (tela.char_x/2, tela.char_y/2, "PAUSADO");
-				while(KEY_data != 2){// Enquanto não apertar o botão continuar, fico no loop
-					KEY_read (&KEY_data);
-				}
-				video_erase();
+        // Veriifico se pressionou o botão de pausar
+    	KEY_read (&botao_valor);
+    	if(botao_valor == 4){// Fico no loop até que pressione o CONTINUAR
+			video_text (tela.char_x/2, tela.char_y/2, "PAUSADO");
+			while(botao_valor != 2){// Enquanto não apertar o botão continuar, fico no loop
+				KEY_read (&botao_valor);
 			}
-    	//video_show(); //Primeiro video_show
+			video_erase();
+		}
+
     	video_clear();
-    	build_cage(&tela, video_color[1]);// Constrói a gaiola
-    	//Apagar a bola antes de movê-la
-    	//video_box(bola.pos_x,bola.pos_y,(bola.pos_x + 1),(bola.pos_y + 1),0);
+        build_cage(&tela, video_color[1]);
     	
-    	//Mover a bola
-        moverBola(&bola);
+    	// --- --- Realiza as movimentações necessárias --- --- //
+        update_pos_Bola(&bola);
+	    update_pos_Barra(&barra, &tela);
 	
-	update_pos_Barra(&barra, &tela);
-	
-	video_box(bola.pos_x,bola.pos_y,(bola.pos_x + 1),(bola.pos_y + 1),bola.color);
-	
-	//Agora faço as checagens antes de desenhá-la na tela        
-        //Checar choque com as laterais
+        // --- --- Verifico se a bola chocou com a gaiola ou perdeu  --- --- //
         if (bola.pos_x <= limite_x1){
             bola.vel[0] = 1;
         } else if((bola.pos_x + 1) >= limite_x2){
@@ -299,18 +289,16 @@ int main(){
         } else if((bola.pos_y) <= limite_y1){
             bola.vel[1] = 1;
         } else if((bola.pos_y + 1) >= limite_y2){
-	    //PERDEU F
-	    	//control = 0;
-	    	// ======================================================================================================
-	    	// TELA DE FIM DE JOGO E REINICIAR
-	    	/// INICIAL LOGO DEPOIS DE CONSTRUIR A TELA
-			KEY_read (&KEY_data);
-			video_text (tela.char_x/3, tela.char_y/2 - 4, "FIM DE JOGO");
-			video_text (tela.char_x/3, tela.char_y/2, "Pressione o botao Iniciar para reiniciar");
-			while(KEY_data != 8){// Só inicia o loop principal quando pressiona o botão 
-				KEY_read (&KEY_data);
+	    	// --- --- TELA DE FIM DE JOGO E REINICIAR --- --- //
+			video_text (tela.char_x/3 - 6, tela.char_y/2 - 4, "FIM DE JOGO");
+			video_text (tela.char_x/3 - 6, tela.char_y/2, "Pressione o botao Iniciar para reiniciar");
+			KEY_read (&botao_valor);
+			while(botao_valor != 8){// Só inicia o loop principal quando pressiona o botão 
+				KEY_read (&botao_valor);
 			}
 			video_erase();
+
+            // ==== Processo de reconstruir a tela, e disponibilizar para jogar novamente ==== //
 			// Limpo qualquer coisa que possa ter na tela
 			video_erase();
 			video_clear ( ); // clear current VGA Back buffer
@@ -340,7 +328,7 @@ int main(){
 			bola.pos_y =  tela.screen_y - 100;// Posição fixa em y 
 			
 			
-
+            // Gerando os blocos nas posições iniciais
 			cont = 1;
 			vetor[0].pos_x1 = 1;
 			vetor[0].pos_y1 = 1;
@@ -372,13 +360,12 @@ int main(){
 				 }
 				 
 			}
+        }
+        ///===========================================================================================
 
-	    	///===========================================================================================
-        } 
-
+        // Trecho que escreve os blocos
         for (i = 0; i < numBlocos; i++){
             //printf("Bloco: %i, X1:%i, Y1:%i, X2:%i, Y2:%i \n", i, vetor[i].pos_x1, vetor[i].pos_y1, vetor[i].pos_x2, vetor[i].pos_y2);
-            
             if(vetor[i].existe){
 				
                 if (((bola.pos_x >= (vetor[i].pos_x1) && bola.pos_x <= (vetor[i].pos_x2)) && 
@@ -391,20 +378,12 @@ int main(){
                 }
                 video_box(vetor[i].pos_x1,vetor[i].pos_y1,vetor[i].pos_x2,vetor[i].pos_y2,vetor[i].color);
             }
-            
         }
 
         if (bola.pos_x >= barra.pos_x1 && bola.pos_x <= barra.pos_x2 && (bola.pos_y+1) == barra.pos_y){
             acelBolaBarra(&barra, &bola);
-            //bola.pos_x = 160;//rand() % (tela.screen_x-2);// Gera a bola em uma posição aleatória no eixo x
-    	    //bola.pos_y =  tela.screen_y - 100;// Posição fixa em y 
-            //bola.vel[0] = 0;
-            //bola.vel[1] = -1;
         }
-        /*
-        printf("Tamanho tela em x=%i y=%i\n", tela.screen_x, tela.screen_y); 
-        printf("Velocidade: X: %d Y: %d\n", bola.vel[0], bola.vel[1]);
-        printf("Posicao: X: %d Y: %d\n", bola.pos_x, bola.pos_y);*/
+
         video_show();
     }
 
